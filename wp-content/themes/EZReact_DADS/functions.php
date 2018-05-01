13df2e87656561ab37528dc78b85e54f90f61d6e
Anton Drake <drakeanddrakeservices@gmail.com>
2018-05-01 18:34:12 -0400

    changed local root path

---
+++ wp-content/themes/EZReact_DADS/functions.php
@@ -0,0 +1,7 @@
+<?php
+
+	// load scripts and styles
+
+	add_action("wp_enqueue_scripts","scriptsAndStyles");
+
+?>
---
+++ wp-content/themes/EZReact_DADS/js/components/home.js
@@ -0,0 +1,8 @@
+import React, {Component} from "react"
+
+
+const Home = ()=>{
+	return <h1>Welcome in Anton</h1>
+}
+
+export default Home
